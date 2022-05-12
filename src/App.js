9aeeca57907d4594c9e5d12f09c6a6422a440c3c
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-05-12 18:02:32 +0200

    Add Route for particular pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import About from "./components/pages/About";
+import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
+import Post from "./components/pages/Post";
+import PostAdd from "./components/pages/PostAdd";
+import PostEdit from "./components/pages/PostEdit";
+
 function App() {
   return (
-    <div className="App">
-      <p>Hello World!</p>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/post/:id" element={<Post />} />
+      <Route path="/post/add" element={<PostAdd />} />
+      <Route path="/post/edit/:id" element={<PostEdit />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
