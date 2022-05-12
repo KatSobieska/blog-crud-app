cefde43935d76346e544d4e38b5a5c98957b6655
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-05-12 18:39:55 +0200

    Import Container, Header and Footer and use it in structure

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Container } from "react-bootstrap";
 import { Route, Routes } from "react-router-dom";
 import About from "./components/pages/About";
 import Home from "./components/pages/Home";
@@ -5,17 +6,23 @@ import NotFound from "./components/pages/NotFound";
 import Post from "./components/pages/Post";
 import PostAdd from "./components/pages/PostAdd";
 import PostEdit from "./components/pages/PostEdit";
+import Footer from "./components/views/Footer";
+import Header from "./components/views/Header";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/post/:id" element={<Post />} />
-      <Route path="/post/add" element={<PostAdd />} />
-      <Route path="/post/edit/:id" element={<PostEdit />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <Container>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/post/:id" element={<Post />} />
+        <Route path="/post/add" element={<PostAdd />} />
+        <Route path="/post/edit/:id" element={<PostEdit />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </Container>
   );
 }
 
