62060b828e4cfb2d5cd6549674f3d680b4d157f1
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-05-12 19:37:13 +0200

    Add example posts to the initialState

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,5 +1,38 @@
 const initialState = {
-  posts: [],
+  posts: [
+    {
+      id: "1",
+      title: "Article title",
+      shortDescription: "Short description of the article...",
+      content: "Main content of the article",
+      publishedDate: "02-02-2022",
+      author: "John Doe",
+    },
+    {
+      id: "2",
+      title: "Article title",
+      shortDescription: "Short description of the article...",
+      content: "Main content of the article",
+      publishedDate: "02-02-2022",
+      author: "Anna More",
+    },
+    {
+      id: "3",
+      title: "Article title",
+      shortDescription: "Short description of the article...",
+      content: "Main content of the article",
+      publishedDate: "02-02-2022",
+      author: "Kevin Stan",
+    },
+    {
+      id: "4",
+      title: "Article title",
+      shortDescription: "Short description of the article...",
+      content: "Main content of the article",
+      publishedDate: "02-02-2022",
+      author: "Ivan Kurt",
+    },
+  ],
 };
 
 export default initialState;
