0d406c3f67a5263e1cc55bd4f4a22000cc1aede7
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-05-12 20:04:20 +0200

    Add structure and styles for Home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,18 @@
+import { Button, Col, Row } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+
 const Home = () => {
   return (
-    <div>
-      <p>Home</p>
-    </div>
+    <Container>
+      <Row>
+        <Col className="d-flex justify-content-start">
+          <h2>Blog.app</h2>
+        </Col>
+        <Col className="d-flex justify-content-end">
+          <Button variant="outline-info">Add post</Button>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
