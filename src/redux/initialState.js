292a980824118012a465feeea3e2f5fbbbe2c60a
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-05-13 22:06:14 +0200

    Add number for article title

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,30 +1,30 @@
 const initialState = {
-  posts: [
-    {
-      id: "1",
-      title: "Article title",
-      shortDescription: "Short description of the article...",
-      content: "Main content of the article",
-      publishedDate: "02-02-2022",
-      author: "John Doe",
-    },
-    {
-      id: "2",
-      title: "Article title",
-      shortDescription: "Short description of the article...",
-      content: "Main content of the article",
-      publishedDate: "02-02-2022",
-      author: "Anna More",
-    },
-    {
-      id: "3",
-      title: "Article title",
-      shortDescription: "Short description of the article...",
-      content: "Main content of the article",
-      publishedDate: "02-02-2022",
-      author: "Kevin Stan",
-    },
-  ],
+    posts: [
+        {
+            id: "1",
+            title: "Article title 1",
+            shortDescription: "Short description of the article...",
+            content: "Main content of the article",
+            publishedDate: "02-02-2022",
+            author: "John Doe",
+        },
+        {
+            id: "2",
+            title: "Article title 2",
+            shortDescription: "Short description of the article...",
+            content: "Main content of the article",
+            publishedDate: "02-02-2022",
+            author: "Anna More",
+        },
+        {
+            id: "3",
+            title: "Article title 3",
+            shortDescription: "Short description of the article...",
+            content: "Main content of the article",
+            publishedDate: "02-02-2022",
+            author: "Kevin Stan",
+        },
+    ],
 };
 
 export default initialState;
