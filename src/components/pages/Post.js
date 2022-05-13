860f75646a7fb4d9cd9453c343b02253c9497429
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-05-13 22:05:46 +0200

    Add postData and create structure for particular post

--- src/components/pages/Post.js
+++ src/components/pages/Post.js
@@ -1,7 +1,46 @@
+import { Col, Row, Button } from "react-bootstrap";
+import { useParams } from "react-router";
+import { useSelector } from "react-redux";
+import { getPostById } from "../../redux/postsRedux";
+
 const Post = () => {
+  const { postId } = useParams();
+  const postData = useSelector((state) => getPostById(state, postId));
+
   return (
     <div>
-      <p>POST</p>
+      <Row className="justify-content-md-center">
+        <Col md={6} className="d-flex justify-content-between">
+          <h1>{postData.title}</h1>
+          <span>
+            <Button
+              variant="outline-info"
+              size="sd"
+              href={"/post/edit/" + postId}
+              style={{
+                marginRight: "10px",
+              }}
+            >
+              Edit
+            </Button>
+            <Button variant="outline-danger" size="sd">
+              Delete
+            </Button>
+          </span>
+        </Col>
+      </Row>
+      <Row className="justify-content-md-center">
+        <Col md={6}>
+          <p>
+            <b>Author: </b>
+            {postData.author}
+          </p>
+          <p>
+            <b>Published: </b> {postData.publishedDate}
+          </p>
+          <p>{postData.content}</p>
+        </Col>
+      </Row>
     </div>
   );
 };
