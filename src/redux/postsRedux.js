6ee8eebe8a52deb2a3fa17b942f2d44f9f23d15c
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-05-14 09:47:23 +0200

    Add action REMOVE_POST and action creator removePost

--- src/redux/postsRedux.js
+++ src/redux/postsRedux.js
@@ -5,14 +5,21 @@ export const getPostById = ({ posts }, postId) =>
 
 export const getAllPosts = (state) => state.posts;
 
-// actions
+// actions creators
 
 const createActionName = (actionName) => `app/posts/${actionName}`;
 
-// action creators
+export const removePost = (payload) => ({ type: REMOVE_POST, payload });
+
+
+// actions
+
+const REMOVE_POST = createActionName('REMOVE_POST');
 
 const postsReducer = (statePart = [], action) => {
     switch (action.type) {
+        case REMOVE_POST:
+            return statePart.filter(post => post.id !== action.payload)
         default:
             return statePart;
     }
