e33e153e285169be6885e80fdd51e84a320a98d3
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-05-14 13:49:56 +0200

    Add AddPostForm import

--- src/components/pages/PostAdd.js
+++ src/components/pages/PostAdd.js
@@ -1,8 +1,14 @@
+import { Col, Row } from "react-bootstrap";
+import AddPostForm from "../features/AddPostForm";
+
 const PostAdd = () => {
   return (
-    <div>
-      <p>Post ADD</p>
-    </div>
+    <Row className="justify-content-md-center">
+      <Col md={6}>
+        <h2>ADD POST</h2>
+        <AddPostForm />
+      </Col>
+    </Row>
   );
 };
 
