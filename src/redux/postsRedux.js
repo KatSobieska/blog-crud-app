8c30e7b0158ee66eb1f371adff06983104d02843
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-05-14 15:09:19 +0200

    Import shortid, create addPost and action ADD_POST

--- src/redux/postsRedux.js
+++ src/redux/postsRedux.js
@@ -1,7 +1,9 @@
+import shortid from "shortid";
+
 // selectors
 
 export const getPostById = ({ posts }, postId) =>
-    posts.find((post) => post.id === postId);
+  posts.find((post) => post.id === postId);
 
 export const getAllPosts = (state) => state.posts;
 
@@ -10,19 +12,22 @@ export const getAllPosts = (state) => state.posts;
 const createActionName = (actionName) => `app/posts/${actionName}`;
 
 export const removePost = (payload) => ({ type: REMOVE_POST, payload });
-
+export const addPost = (payload) => ({ type: ADD_POST, payload });
 
 // actions
 
-const REMOVE_POST = createActionName('REMOVE_POST');
+const REMOVE_POST = createActionName("REMOVE_POST");
+const ADD_POST = createActionName("ADD_POST");
 
 const postsReducer = (statePart = [], action) => {
-    switch (action.type) {
-        case REMOVE_POST:
-            return statePart.filter(post => post.id !== action.payload)
-        default:
-            return statePart;
-    }
+  switch (action.type) {
+    case REMOVE_POST:
+      return statePart.filter((post) => post.id !== action.payload);
+    case ADD_POST:
+      return [...statePart, { ...action.payload, id: shortid() }];
+    default:
+      return statePart;
+  }
 };
 
 export default postsReducer;
