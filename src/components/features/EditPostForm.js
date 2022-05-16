4d4262b02c04b8a4a3d5eac2aa0e8c794c54497f
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-05-16 19:32:59 +0200

    Add component for post editing

---
+++ src/components/features/EditPostForm.js
@@ -0,0 +1,40 @@
+import { useDispatch, useSelector } from "react-redux";
+import PostForm from "./PostForm";
+import { getPostById } from "../../redux/postsRedux";
+import { useNavigate, useParams } from "react-router";
+import { editPost } from "../../redux/postsRedux";
+import { Navigate } from "react-router";
+import { Row, Col } from "react-bootstrap";
+
+const EditPostForm = () => {
+  const { postId } = useParams();
+  const postData = useSelector((state) => getPostById(state, postId));
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleSubmit = (post) => {
+    dispatch(editPost({ ...post, postId }));
+    navigate("/");
+  };
+
+  if (!postData) return <Navigate to="/" />;
+  return (
+    <Row className="justify-content-md-center">
+      <Col md={6}>
+        <h2>EDIT POST</h2>
+        <PostForm
+          action={handleSubmit}
+          actionText="Edit post"
+          title={postData.title}
+          author={postData.author}
+          publishedDate={postData.publishedDate}
+          shortDescription={postData.shortDescription}
+          content={postData.content}
+          id={postId}
+        />
+      </Col>
+    </Row>
+  );
+};
+
+export default EditPostForm;
