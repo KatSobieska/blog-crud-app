ffd4e51fd4c368885e52d61665e4ab224c2a473a
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-05-16 19:34:29 +0200

    Add action creator editPost and action EDIT_POST

--- src/redux/postsRedux.js
+++ src/redux/postsRedux.js
@@ -13,11 +13,13 @@ const createActionName = (actionName) => `app/posts/${actionName}`;
 
 export const removePost = (payload) => ({ type: REMOVE_POST, payload });
 export const addPost = (payload) => ({ type: ADD_POST, payload });
+export const editPost = (payload) => ({ type: EDIT_POST, payload });
 
 // actions
 
 const REMOVE_POST = createActionName("REMOVE_POST");
 const ADD_POST = createActionName("ADD_POST");
+const EDIT_POST = createActionName("EDIT_POST");
 
 const postsReducer = (statePart = [], action) => {
   switch (action.type) {
@@ -25,6 +27,10 @@ const postsReducer = (statePart = [], action) => {
       return statePart.filter((post) => post.id !== action.payload);
     case ADD_POST:
       return [...statePart, { ...action.payload, id: shortid() }];
+    case EDIT_POST:
+      return statePart.map((post) =>
+        post.id === action.payload.id ? { ...post, ...action.payload } : post
+      );
     default:
       return statePart;
   }
