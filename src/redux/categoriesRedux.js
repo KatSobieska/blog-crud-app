a2e91f273be9bd48e28691bedab3eace64df8ea8
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-05-22 20:37:37 +0200

    Add categoriesReducer and selector getAllCategories

---
+++ src/redux/categoriesRedux.js
@@ -0,0 +1,13 @@
+// selectors
+
+export const getAllCategories = (state) => state.categories;
+
+// actions
+
+const categoriesReducer = (statePart = [], action) => {
+  switch (action.type) {
+    default:
+      return statePart;
+  }
+};
+export default categoriesReducer;
